dea6fa91469c30c164478453d24def494bc66be8
Nixon2223 <n.nixon@hotmail.co.uk>
2022-02-15 08:02:41 +0000

    TobaccoStallTest - vistitor1, visitor2 created

--- ThemeParkStartPoint/src/test/java/stalls/TobaccoStallTest.java
+++ ThemeParkStartPoint/src/test/java/stalls/TobaccoStallTest.java
@@ -2,8 +2,9 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class TobaccoStallTest {
 
@@ -12,6 +13,8 @@ public class TobaccoStallTest {
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        visitor1 = new Visitor(15,200.00,50.00);
+        visitor2 = new Visitor(17,200.00,50.00);
     }
 
     @Test
@@ -28,4 +31,11 @@ public class TobaccoStallTest {
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
     }
+
+    @Test
+    public void canAllow(){
+        assertTrue(playground.isAllowedTo(visitor1));
+        assertFalse(playground.isAllowedTo(visitor2));
+    }
 }
+
